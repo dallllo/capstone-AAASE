825f34110830c0a359d16c661da874b1cc415454
dalal <44893286+dallllo@users.noreply.github.com>
2026-07-23 13:50:42 +0300

    Delete google_map directory

--- google_map/lib/features/home/domain/repositories/home_repository_domain.dart
+++
@@ -1,7 +0,0 @@
-import 'package:multiple_result/multiple_result.dart';
-import 'package:google_map/core/errors/failure.dart';
-import 'package:google_map/features/home/domain/entities/home_entity.dart';
-
-abstract class HomeRepositoryDomain {
-    Future<Result<HomeEntity, Failure>> getHome();
-}
